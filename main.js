c47ac695f5b44f64b5dad7fbe0f60ee0f2abceab
Dalvin Segura <64456007+dalvin181@users.noreply.github.com>
2021-06-11 11:54:20 -0400

    This is a web application that consume data by API with PHP, AJAX, JSON from its own data base MySQL

---
+++ main.js
@@ -0,0 +1,67 @@
+let name = $("name");
+let lastName = $("lastName");
+let email = $("email");
+let pass = $("pass");
+
+
+
+$(function(){
+    listar();
+    guardar();
+});
+
+function listar(){
+    _ajax("showdata.php", "")
+    .done(function(info){
+        // console.log(info);
+
+        var usuarios = JSON.parse(info);
+        // console.log(usuarios);
+        let html = "";
+
+        for(var i in usuarios.data){
+            html+=` <h1> <b>Nombres:</h1> <p>${usuarios.data[i].nombre}</b>
+                    <h1> <b>Apellido: </h1> <p>${usuarios.data[i].apellido}</p></b>
+                    <h1> <b>Correo Electronico: </h1> <p>${usuarios.data[i].correo_electronico}</p></b>
+                    <h1> <b>Contraseña:</h1> <p>${usuarios.data[i].contraseña}</p></b>
+                    <div style="border-bottom: 7px solid green"></div>
+            `;
+        }
+        
+        $("#data_usuarios").html(html);
+    });
+}
+
+function datos(){
+    let data = [];
+    data.push(
+        {"nombre":"nombretest", "apellido":"apellidotest", "correo_electronico":"correotest", "contraseña":"contraseñartest"},
+    );
+
+    let usuarios = {"data": data};
+    return usuarios;
+}
+
+function guardar(){
+    let json = JSON.stringify(datos());
+    _ajax("savedata.php",{"json": json})
+    .done(function(info){
+        if(info){
+            $("#data_enviada").html(`<h3>SE HA REGISTRADO CORRECTAMENTE LOS DATOS</h3>`)
+            
+            listar();
+        }else{
+            $("#data_enviada").html(`<h3>¡ERROR!</h3>`)
+
+        }
+    });
+}
+
+function _ajax(url, data){
+   let ajax = $.ajax({
+        "method": "POST",
+        "url": url,
+        "data": data
+    })
+    return ajax;
+}
---
+++ savedata.php
@@ -0,0 +1,24 @@
+<?php
+
+include'conexion.php';
+
+$sql = "INSERT INTO register (nombre, apellido, correo_electronico, contraseña) VALUES (?,?,?,?)";
+$statement = $dbh->prepare($sql);
+
+
+$usuarios = json_decode($_POST["json"]);
+// var_dump($usuarios->{"data"}[0]->{"nombre"});
+
+$respuesta = false;
+
+foreach($usuarios->{"data"} as $usuario){
+    $statement->bindParam(1, $usuario->{"nombre"}, PDO::PARAM_STR);
+    $statement->bindParam(2, $usuario->{"apellido"}, PDO::PARAM_STR);
+    $statement->bindParam(3, $usuario->{"correo_electronico"}, PDO::PARAM_STR);
+    $statement->bindParam(4, $usuario->{"contraseña"}, PDO::PARAM_STR);
+
+    $respuesta = $statement->execute();
+};
+
+echo $respuesta;
+?>
